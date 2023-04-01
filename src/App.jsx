056960ecaf80edec341436b81317532027fb05e2
Logan Faschingbauer <faschingbauerl@yahoo.com>
2023-03-31 23:35:38 -0500

    add switch to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./css/reset.css";
 import "./css/styles.css";
+import { useState } from "react";
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
+import { Switch } from "./components/Switch";
 
 function App() {
+  const [isYearlyBilling, setIsYearlyBilling] = useState(false);
+
+  const handleYearlyBillingToggle = () => {
+    setIsYearlyBilling(!isYearlyBilling);
+  };
+
   return (
     <div className="App">
       <Header
@@ -24,7 +32,11 @@ function App() {
           </div>
           <div className="card__billing">
             <div>Monthly Billing</div>
-            <div>PLACEHOLDER_SWITCH</div>
+            <Switch
+              isOn={isYearlyBilling}
+              label="yearly-billing"
+              handleToggle={handleYearlyBillingToggle}
+            />
             <div>Yearly Billing</div>
             <div className="card__pill">-25%</div>
           </div>
---
+++ src/components/Switch.jsx
@@ -0,0 +1,21 @@
+// from https://upmostly.com/tutorials/build-a-react-switch-toggle-component
+
+export const Switch = ({ isOn, handleToggle, label }) => {
+  return (
+    <>
+      <input
+        className="switch__checkbox"
+        checked={isOn}
+        onChange={handleToggle}
+        id={label}
+        type="checkbox"
+      />
+      <label
+        className={isOn ? "switch__label switch__label--on" : "switch__label"}
+        htmlFor={label}
+      >
+        <span className="switch__button" />
+      </label>
+    </>
+  );
+};
